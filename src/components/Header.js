2b25140527fc918ae82d673a471bed44d348b7c0
Ryan Howard <ryanghoward@protonmail.com>
2022-01-19 15:28:06 -0800

    Header component built and rendering

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>You bored bro?</h1>
+      <h4>Let's find something for you to do!</h4>
+    </div>
+  );
+};
+
+export default Header;
