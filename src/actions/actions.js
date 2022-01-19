f5438631b9ebab7098b1937feb989726e6c6dd93
Ryan Howard <ryanghoward@protonmail.com>
2022-01-19 15:46:21 -0800

    Actions and Reducers built

---
+++ src/actions/actions.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAIL = "FETCH_FAIL";
+
+export const fetchStart = () => {
+  return { type: FETCH_START };
+};
+
+export const fetchSuccess = (activities) => {
+  return { type: FETCH_SUCCESS, payload: activities };
+};
+
+export const fetchFail = (error) => {
+  return { type: FETCH_FAIL, payload: error };
+};
---
+++ src/reducers/reducers.js
@@ -0,0 +1,37 @@
+import { FETCH_FAIL, FETCH_START, FETCH_SUCCESS } from "../actions/actions";
+
+const initState = {
+  activity: "",
+  isFetching: false,
+  error: "",
+};
+
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    case FETCH_START:
+      return {
+        activity: "",
+        isFetching: true,
+        error: "",
+      };
+
+    case FETCH_SUCCESS:
+      return {
+        activity: action.payload.activity,
+        isFetching: false,
+        error: "",
+      };
+
+    case FETCH_FAIL:
+      return {
+        activity: "",
+        isFetching: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
